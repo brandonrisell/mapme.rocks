22b5e06d9a424e18d86feab04b1532df0835e280
Mat Wood <mat@thepacketgeek.com>
2015-01-24 16:42:55 -0800

    Improving layout, styling, and sizing.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from flask import Flask, request, jsonify, render_template, flash, g
+from flask import Flask, request, jsonify, render_template, flash, g, redirect, url_for
 import json, socket, geoip2.database, geoip2.errors, ipaddr, os
 from flask_bootstrap import Bootstrap
 import rethinkdb as r
@@ -47,82 +47,86 @@ def teardown_request(exception):
 
 def get_traits(ip_addr):
 
-	try:
-		ipaddr.IPAddress(ip_addr)
-		domain_name = socket.gethostbyaddr(ip_addr)[0]
-	except (ValueError, ipaddr.AddressValueError) as e:
-		domain_name = ip_addr
-		ip_addr = socket.gethostbyname(domain_name)
+    try:
+        ipaddr.IPAddress(ip_addr)
+        domain_name = socket.gethostbyaddr(ip_addr)[0]
+    except (ValueError, ipaddr.AddressValueError) as e:
+        domain_name = ip_addr
+        ip_addr = socket.gethostbyname(domain_name)
 
-	traits = reader.city(ip_addr)
+    traits = reader.city(ip_addr)
 
-	details = {
-			'IP Address': ip_addr,
-			'City': traits.city.name,
-			'Subdivision' : traits.subdivisions.most_specific.name,
-			'Country': traits.country.name,
-			'Country Code' : traits.country.iso_code,
-			'Latitude' : traits.location.latitude,
-			'Longitude' : traits.location.longitude,
-			'Domain Name' : domain_name
-			}
+    details = {
+            'IP Address': ip_addr,
+            'City': traits.city.name,
+            'Subdivision' : traits.subdivisions.most_specific.name,
+            'Country': traits.country.name,
+            'Country Code' : traits.country.iso_code,
+            'Latitude' : traits.location.latitude,
+            'Longitude' : traits.location.longitude,
+            'Domain Name' : domain_name
+            }
 
-	# store query for history
-	inserted = r.table('queries').insert(details).run(g.rdb_conn)
+    # store query for history
+    inserted = r.table('queries').insert(details).run(g.rdb_conn)
 
-	return details
+    return details
 
 @app.route('/')
 def home():
 
-	return render_template('home.html')
+    return render_template('home.html')
 
 @app.route('/dest/<destination>')
 def home_dest(destination):
 
-	try:
-		ip_traits = get_traits(destination)
-	except ValueError as e:
-		return jsonify({'Error': e.message}), 400
+    try:
+        ip_traits = get_traits(destination)
+    except ValueError as e:
+        return jsonify({'Error': e.message}), 400
 
-	try:
-		return render_template('home.html', destination=get_traits(destination))		
-	except ValueError as e:
-		flash(e.message, 'danger')
-		return render_template('home.html')		
+    try:
+        return render_template('home.html', destination=get_traits(destination), orig_query=destination)        
+    except ValueError as e:
+        flash(e.message, 'danger')
+        return render_template('home.html')     
 
 @app.route('/feed/')
 def feed():
 
-	try:
-		limit = int(request.args.get('limit')) or 20
-	except TypeError:
-		limit = 20
-		
-	points = [x for x in r.table('queries').limit(limit).run(g.rdb_conn)]
+    try:
+        limit = int(request.args.get('limit')) or 20
+    except TypeError:
+        limit = 20
+        
+    points = [x for x in r.table('queries').limit(limit).run(g.rdb_conn)]
 
-	return render_template('tail.html', points=points, limit=limit)
+    return render_template('tail.html', points=points, limit=limit)
 
 @app.route('/findme')
 def findme():
 
-	return render_template('findme.html')
+    return render_template('findme.html')
 
 @app.route('/features')
 def features():
 
-	return render_template('features.html')
+    return render_template('features.html')
 
 @app.route('/geoip/<ip_addr>', methods=['GET', 'POST'])
 def geoip_lookup(ip_addr):
-	
-	try:
-		details = get_traits(ip_addr)
-	except Exception as e:
-		return jsonify({"Error": "'" + ip_addr + "' is not a valid domain name or IP address."}), 400
+    
+    try:
+        details = get_traits(ip_addr)
+    except Exception as e:
+        return jsonify({"Error": "'" + ip_addr + "' is not a valid domain name or IP address."}), 400
 
-	return jsonify(details), 200
+    return jsonify(details), 200
 
+@app.errorhandler(404)
+def page_not_found(e):
+    flash('The page you requested does not exist', 'warning')
+    return redirect(url_for('home'))
 
 if __name__ == '__main__': 
-	app.run(host='0.0.0.0', debug=True, port=5000)
+    app.run(host='0.0.0.0', debug=True, port=5000)
