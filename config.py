67fda9ea663d54f0244868cabb34823e22fa3f9e
Mat Wood <mat@thepacketgeek.com>
2015-01-31 18:22:24 -0800

    Fixing DB name value
    Mongolab uses it's own DB name

--- config.py
+++ config.py
@@ -3,7 +3,10 @@ import urlparse, os
 class Config:
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SECRET_KEY = 'BPQ4kMHJPaC6bCzC'
-    MONGOLAB_DB = 'mapme'
     MONGOLAB_URI = os.environ.get('MONGOLAB_URI')
-    if not MONGOLAB_URI:
+    if MONGOLAB_URI:
+        url = urlparse.urlparse(MONGOLAB_URI)
+        MONGOLAB_DB = url.path[1:]
+    else:  #if no env settings, use localhost (for dev)
         MONGOLAB_URI = 'mongodb://localhost:27017/mapme'
+        MONGOLAB_DB = 'mapme'
